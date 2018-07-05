604af1391d8553bab2bb0429eb06f6089fe14b78
emChristos <oriahie@gmail.com>
2018-07-05 15:32:30 +0100

    [emChristos] Edit Modal Created..Gaby isup to you

--- Chopwella.Web/Controllers/AdminController.cs
+++ Chopwella.Web/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Chopwella.Core;
 using Chopwella.ServiceInterface;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace Chopwella.Web.Controllers
@@ -7,14 +8,21 @@ namespace Chopwella.Web.Controllers
     public class AdminController : Controller
     {
         private readonly IServices<Category> catservice;
+        private readonly IServices<Staff> staffservice;
+        private readonly IServices<Vendor> vendorservice;
 
-        public AdminController(IServices<Category> catservice)
+        public AdminController(IServices<Category> catservice, IServices<Staff> staffservice, IServices<Vendor> vendorservice)
         {
             this.catservice = catservice;
+            this.staffservice = staffservice;
+            this.vendorservice = vendorservice;
         }
         // GET: Admin
         public ActionResult Index()
         {
+            ViewBag.StaffCount = staffservice.GetAll().Count();
+            ViewBag.CatCount = catservice.GetAll().Count();
+            ViewBag.VenCount = vendorservice.GetAll().Count();
             return View();
         }
         public ActionResult Staff()
